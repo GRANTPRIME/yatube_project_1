b6f844af1c71365bf8a800a2eede0cf7daa770d8
Илья Гудков <goodock@yandex.ru>
2022-09-14 21:39:44 +0300

    пилотный проект

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts_list'
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts),
+    path('', views.index, name='posts_list'),
+    path('group/<slug:slug>/', views.group_posts, name='group_posts_list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,46 @@
+from multiprocessing import context
 from django.http import HttpResponse
-from django.shortcuts import render
 # Create your views here.
+from .models import Post, Group
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return HttpResponse('Главная страница')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    template = 'posts/index.html'
+    return render(request, template, context)
 
-def group_posts(request, pk):
-    return HttpResponse(f'Список групп {pk}')
+def group_posts(request, slug):
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
+
+#def group_posts(request, slug):
+    #group = get_object_or_404(Group, slug=slug)
+    #posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+ #   posts = Post.objects.order_by('-pub_date')[:10]
+  #  context = {
+      #  'group': group,
+   #     'posts': posts,
+      #  'text': 'Лев Толстой - не хер простой'
+    #}
+    #context = {
+     #   'text': 'Лев Толстой - не хер простой'
+    #}
+    #template = 'posts/group_list.html'
+    #return HttpResponse(f'Список групп {slug}')
+    #return render(request, template, context)
